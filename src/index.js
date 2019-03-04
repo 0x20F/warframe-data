f380aeb93c74290875057ad616d2dec98b5a9344
Poly <alexhexan22@gmail.com>
2019-03-04 17:36:52 +0100

    Add navbar icons

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import ReactDOM     from "react-dom";
 
 // Fontawesome icons
 import { library }  from "@fortawesome/fontawesome-svg-core";
-import { faBars, faTimes }   from "@fortawesome/free-solid-svg-icons";
+import { faNewspaper, faHome, faExclamationTriangle }   from "@fortawesome/free-solid-svg-icons";
 /**
  * Icon sizes:
  *  lg, xs, sm
  *  1x, 2x, 3x, 4x, 5x
  *  Add them as a size="" attribute on the component
  */
-library.add(faBars, faTimes);
+library.add(faNewspaper, faHome, faExclamationTriangle);
 
 
 
