8e5d049cf519e5d5598f35bf299ad491dc9764a4
Poly <alexhexan22@gmail.com>
2019-03-04 10:14:05 +0100

    Add news path

--- src/app/AppRouter.jsx
+++ src/app/AppRouter.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { Component }                         from "react";
+import { BrowserRouter as Router, Route, Link }     from "react-router-dom";
 
 
-import Home from "@Pages/Home";
+import Home     from "@Pages/Home";
+import News     from "@Pages/News";
 
-import Navbar from "@Components/Navbar";
+import Navbar   from "@Components/Navbar";
 
 
 class AppRouter extends Component {
@@ -14,8 +15,9 @@ class AppRouter extends Component {
             <Router>
                 <div>
                     <Navbar/>
-                    
-                    <Route path="/" component={Home}/>
+
+                    <Route path="/" exact component={Home} />
+                    <Route path="/news" component={News} />
                 </div>
             </Router>
         );
