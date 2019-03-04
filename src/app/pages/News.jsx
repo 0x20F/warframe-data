2c4f1a9546dc605809864970fe2db400fff4320e
Poly <alexhexan22@gmail.com>
2019-03-04 10:13:38 +0100

    Add news Component

---
+++ src/app/pages/News.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+
+class News extends Component {
+
+    render() {
+        return (
+            <div>
+                Here you see important news
+            </div>
+        );
+    }
+}
+
+
+export default News;
