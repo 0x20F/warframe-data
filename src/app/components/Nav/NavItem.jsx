8e030a09af2f509f9d852465723387ea5ff9d1e5
Poly <alexhexan22@gmail.com>
2019-03-06 19:32:14 +0100

    Scrap the grid system

--- src/app/components/Nav/NavItem.jsx
+++ src/app/components/Nav/NavItem.jsx
@@ -2,11 +2,14 @@ import React                    from "react";
 import { FontAwesomeIcon }      from "@fortawesome/react-fontawesome";
 import { NavLink }              from "react-router-dom";
 
+import "@Styles/components/navitem";
+
 
 const NavItem = (props) => {
     return (
         <NavLink
             to={props.destination} 
+            className="nav-item"
             activeClassName="active"
             exact={true}>
 
--- src/app/pages/News.jsx
+++ src/app/pages/News.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 
-import GridItem from "@Components/General/GridItem";
 import "@Styles/pages/news";
 
 
@@ -11,23 +10,11 @@ class News extends Component {
         const featuredSize  = 3; // How many items will go in the grid
         const newsData      = this.props.news;
         const elements      = newsData.reverse().slice(0, featuredSize).map((item, index) => {
-            let cName = "";
-
-            if (index === 0) {
-                cName = "most-recent";
-            }
-
-            return (
-                <GridItem 
-                    className={cName} 
-                    key={item.id} 
-                    data={item}/>
-            );
+            
         });
         
         return (
             <div className="news-container">
-                <header>News</header>
                 <div className="wrapper">
                     {elements}
                 </div>
