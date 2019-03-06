13017edf359eb7f26c9934c20c1c0cddc1918045
Poly <alexhexan22@gmail.com>
2019-03-06 16:58:16 +0100

    Make grid more responsive

--- src/app/pages/News.jsx
+++ src/app/pages/News.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-
+import GridItem from "@Components/General/GridItem";
 import "@Styles/pages/news";
 
 
@@ -10,18 +10,24 @@ class News extends Component {
     render() {
         const featuredSize  = 3; // How many items will go in the grid
         const newsData      = this.props.news;
-        const elements      = newsData.slice(0, featuredSize).map((item, index) => {
+        const elements      = newsData.reverse().slice(0, featuredSize).map((item, index) => {
+            let cName = "";
+
             if (index === 0) {
-                // TODO: THE DIV NEEDS TO BE ITS OWN COMPONENT
-                return <div className="most-recent" key={item.id}> {item.message} </div>;
+                cName = "most-recent";
             }
 
-            return <div key={item.id}> {item.message} </div>
+            return (
+                <GridItem 
+                    className={cName} 
+                    key={item.id} 
+                    data={item}/>
+            );
         });
         
         return (
             <div className="news-container">
-                <h1>News</h1>
+                <header>News</header>
                 <div className="wrapper">
                     {elements}
                 </div>
