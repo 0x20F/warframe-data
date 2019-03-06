ac0653277d2eb37ea1a9652b4fc733c6fbf9264d
Poly <alexhexan22@gmail.com>
2019-03-06 15:49:05 +0100

    Make use of the "news" data

--- src/app/pages/News.jsx
+++ src/app/pages/News.jsx
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
 
 
+import "@Styles/pages/news";
+
+
 class News extends Component {
 
 
     render() {
+        const featuredSize  = 3; // How many items will go in the grid
+        const newsData      = this.props.news;
+        const elements      = newsData.slice(0, featuredSize).map((item, index) => {
+            if (index === 0) {
+                // TODO: THE DIV NEEDS TO BE ITS OWN COMPONENT
+                return <div className="most-recent" key={item.id}> {item.message} </div>;
+            }
+
+            return <div key={item.id}> {item.message} </div>
+        });
         
         return (
-            <div>
-                Welcome to the news page
+            <div className="news-container">
+                <h1>News</h1>
+                <div className="wrapper">
+                    {elements}
+                </div>
             </div>
         );
     }
