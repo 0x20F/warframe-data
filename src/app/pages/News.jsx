4318baf97a8aec8d4f9bc5b8929c80d8f58f665b
Poly <alexhexan22@gmail.com>
2019-03-09 18:41:31 +0100

    Cleanup, redesign in progress

--- src/app/pages/News.jsx
+++ src/app/pages/News.jsx
@@ -7,18 +7,10 @@ class News extends Component {
 
 
     render() {
-        const featuredSize  = 3; // How many items will go in the grid
-        const newsData      = this.props.news;
-        const elements      = newsData.reverse().slice(0, featuredSize).map((item, index) => {
-            // Reversed array because the newest items are last
-            return <div key={item.id}>{item.message}</div>
-        });
-        
+
         return (
-            <div className="news-container">
-                <div className="wrapper">
-                    {elements}
-                </div>
+            <div>
+                Code here
             </div>
         );
     }
