7c9de0d9efb98bffe6d27d3f912413bbee956fb9
Poly <alexhexan22@gmail.com>
2019-03-10 21:28:55 +0100

    Cleanup so its easier to focus on one thing

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -26,57 +26,6 @@ class Home extends Component {
                         placeholder
                     </div>
                 </div>
-
-                <div>
-                    <div className="top">
-                        <header>Baro Ki'Teer</header>
-                        <div className="subtitle">Last visit was 9 hours ago</div>
-                    </div>
-                    <div className="bottom">
-                        <span className="info">20h</span>
-                        <span className="detail">ETA</span>
-                    </div>
-                </div>
-
-                <div>
-                    <div className="top">
-                        <header>Placeholder</header>
-                        <div className="subtitle">This is some extra info</div>
-                    </div>
-                    <div className="bottom">
-                        placeholder
-                    </div>
-                </div>
-
-                <div>
-                    <div className="top">
-                        <header>Placeholder</header>
-                        <div className="subtitle">This is some extra info</div>
-                    </div>
-                    <div className="bottom">
-                        placeholder
-                    </div>
-                </div>
-
-                <div>
-                    <div className="top">
-                        <header>Placeholder</header>
-                        <div className="subtitle">This is some extra info</div>
-                    </div>
-                    <div className="bottom">
-                        placeholder
-                    </div>
-                </div>
-
-                <div>
-                    <div className="top">
-                        <header>Placeholder</header>
-                        <div className="subtitle">This is some extra info</div>
-                    </div>
-                    <div className="bottom">
-                        placeholder
-                    </div>
-                </div>
             </div>
         );
     }
