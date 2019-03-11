a5bf550060e0adf940923ae207865dda673be607
Poly <alexhexan22@gmail.com>
2019-03-11 19:29:25 +0100

    Add default values to the component for testing

--- src/app/components/General/SmallGridItem.jsx
+++ src/app/components/General/SmallGridItem.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 
+import "@Styles/components/smallgriditem";
+
 
 const SmallGridItem = (props) => {
     return (
-        <div>
-            <div className="top">
-                <header>{ props.title }</header>
-                <div className="subtitle">{ props.subtitle }</div>
+        <div className="small-container">
+            <div className="info">
+                
+                <div className="top">
+                    <header>{ props.title }</header>
+                    <div className="subtitle">{ props.subtitle }</div>
+                </div>
+
+                <div className="bottom">bottom text</div>
+
             </div>
-            <div className="bottom">
-                placeholder
+
+            <div className="live-data">
+                {/* Nothing will go here unless state I guess?*/}
             </div>
         </div>
     );
