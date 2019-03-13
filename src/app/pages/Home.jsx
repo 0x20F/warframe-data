e41f5a342769acff533b5bfcd118294ab82e8894
Poly <alexhexan22@gmail.com>
2019-03-13 18:33:09 +0100

    Basic design idea for most recent news

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -7,16 +7,29 @@ import "@Styles/pages/home";
 
 
 class Home extends Component {
+
     render() {
+        const news          = this.props.news.reverse();
+        const mostRecent    = news[1];
+        const truncate      = (s) => {
+            return (s.length > 30) ? s.substr(0, 29) + "..." : s;
+        }
+
         return (
             <React.Fragment>
                 
                 <div className="background"></div>
 
                 <div className="grid">
-                    <div className="news">
-                        This is the news component
-                    </div>
+
+                    <a href={ mostRecent.link } className="news">
+                        <div className="indicator">breaking news</div>
+                        <img className="article-image" src={ mostRecent.imageLink }/>
+                        <footer>
+                            <header>{ truncate(mostRecent.message) }</header>
+                            <span className="time">{ mostRecent.eta }</span>
+                        </footer>
+                    </a>
 
                     <div className="alerts">
                         Here we see very important alerts
