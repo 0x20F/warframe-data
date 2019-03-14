e92f3ef950bba851fc7db8f773991b66addb7eb9
Poly <alexhexan22@gmail.com>
2019-03-14 18:36:56 +0100

    Create navbar and use new style

---
+++ src/app/components/Global/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "@Styles/components/navbar";
+
+
+const Navbar = (props) => {
+
+    return (
+        <nav>
+            <header>Grid</header>
+        </nav>
+    );
+}
+
+
+export default Navbar;
