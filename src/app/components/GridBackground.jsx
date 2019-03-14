7c2960d69a8462b3727903fd77ad96611aa4745c
Poly <alexhexan22@gmail.com>
2019-03-14 15:21:51 +0100

    Create a more interesting background

---
+++ src/app/components/GridBackground.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import "@Styles/components/gridbackground";
+
+
+
+// For the lines going out of the corners
+const coords = {
+    b_left  : [
+        {x1: "0", y1: "100%", x2: "100%", y2: "0"},
+        {x1: "0", y1: "100%", x2: "50%", y2: "0"},
+        {x1: "0", y1: "100%", x2: "100%", y2: "50%"}
+    ],
+    b_right : [
+        {x1: "100%", y1: "100%", x2: "0", y2: "0"},
+        {x1: "100%", y1: "100%", x2: "0", y2: "50%"},
+        {x1: "100%", y1: "100%", x2: "50%", y2: "0"}
+    ],
+    t_left  : [
+        {x1: "0", y1: "0", x2: "100%", y2: "50%"},
+        {x1: "0", y1: "0", x2: "50%", y2: "100%"}
+    ],
+    t_right : [
+        {x1: "100%", y1: "0", x2: "0", y2: "50%"},
+        {x1: "100%", y1: "0", x2: "50%", y2: "100%"}
+    ]
+}
+
+
+
+const GridBackground = () => {
+
+    const svgLines = Object.keys(coords).map((key, index) => {
+        return coords[key].map((c, i) => {
+            return (
+                <svg key={ index + i }>
+                    <line x1={ c.x1 } y1={ c.y1 } x2={ c.x2 } y2={ c.y2 }/>
+                </svg>
+            )
+        });
+    });
+
+
+    return (
+        <div className="grid-background">
+            { svgLines }
+        </div>
+    );
+}
+
+export default GridBackground;
