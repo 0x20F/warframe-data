28634cb77b2d09e3fac0f56685fd9224fb01c434
Poly <alexhexan22@gmail.com>
2019-03-14 11:53:24 +0100

    Cleanup

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 
-import SmallGridItem from "@Components/General/SmallGridItem";
-
 import "@Styles/pages/home";
 
 
@@ -9,40 +7,12 @@ import "@Styles/pages/home";
 class Home extends Component {
 
     render() {
-        const news          = this.props.news.reverse();
-        const mostRecent    = news[1];
-        const truncate      = (s) => {
-            return (s.length > 30) ? s.substr(0, 29) + "..." : s;
-        }
-
 
         return (
-            <React.Fragment>
-
-                <div className="grid">
-
-                    {/* TODO: Move into it's own component */}
-                    <a href={ mostRecent.link } className="news">
-                        <div className="indicator">breaking news</div>
-                        <img className="article-image" src={ mostRecent.imageLink }/>
-                        <footer>
-                            <header>{ truncate(mostRecent.message) }</header>
-                            <span className="time">{ mostRecent.eta /* Temporary, needs formatting */}</span>
-                        </footer>
-                    </a>
-
-                    <div className="alerts">
-                        Here we see very important alerts
-                    </div>
+            <div className="grid">
 
 
-                
-                    <SmallGridItem title="placeholder" subtitle="placeholder subtitle"/>
-                  
-
-                </div>
-                
-            </React.Fragment>
+            </div>
         );
     }
 }
