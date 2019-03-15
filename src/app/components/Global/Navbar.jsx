acb5dcc23d4d954b310b430a5e7a6106cdbd2973
Poly <alexhexan22@gmail.com>
2019-03-15 18:03:52 +0100

    More mobile friendly navbar

--- src/app/components/Global/Navbar.jsx
+++ src/app/components/Global/Navbar.jsx
@@ -9,24 +9,54 @@ class Navbar extends Component {
     constructor() {
         super();
 
-        this.links = {
+        this.links = { // Update this later on I guess
             home: "/",
             news: "/news"
         };
+
+        this.state = {
+            sidebarActive: false
+        }
+
+        this.handleBurgerClick = this.handleBurgerClick.bind(this);
     }
 
+    // Handle the state change on burger menu click
+    handleBurgerClick() {
+        this.setState(state => {
+            return {
+                sidebarActive: !state.sidebarActive
+            }
+        });
+    }
+
+
+
     render() {
+        // Build an array of all require links
         const header    = this.props.header.toLowerCase();
         const elements  = Object.keys(this.links).map((key, index) => {
-            if(key === header) return; 
+            if(key === header) return; // If the key is the current page, pass
 
             return <Link to={this.links[key]} key={index}> {key} </Link>
         });
 
+        // Have easier access to the state variable
+        const active    = this.state.sidebarActive;
+
+
+
         return (
             <nav>
                 <header>{ header }</header>
-                {elements}
+                <div className="quick-nav">{ elements }</div>
+                <div 
+                    className={`burger ${active ? "active" : ""}`}
+                    onClick={this.handleBurgerClick} >
+
+                    <span className="icon"></span>
+
+                </div>
             </nav>
         );
     }
