5e21293d7dbde25bd625fec66af7f0c0365d2103
Poly <alexhexan22@gmail.com>
2019-03-16 18:22:46 +0100

    Create a component for dev related information

---
+++ src/app/dev/DevInfo.jsx
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+
+import "@Dev/devinfostyles";
+
+
+class DevInfo extends Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            height: window.innerHeight,
+            width: window.innerWidth,
+            hidden: false
+        };
+        this.sizes = {
+            phone: 600,
+            tablet: 900,
+            desktop: 1200,
+            desktopPlus: 1800
+        }
+        this.update = this.update.bind(this);
+        this.hideElement = this.hideElement.bind(this);
+        this.currentColor = this.currentColor.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.update);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.update);
+    }
+
+    // Update the state width and height
+    update() {
+        this.setState({
+            height: window.innerHeight,
+            width: window.innerWidth
+        });
+    }
+
+    // Update whether or not element should be hidden
+    hideElement() {
+        this.setState(state => {
+            return {
+                hidden: !state.hidden
+            }
+        });
+    }
+
+    // Set the color based on width
+    currentColor() {
+        const colors = {phone: "#2d7dd2", tablet: "#97cc04", desktop: "#f8333c", desktopPlus: "#000"};
+        const keys = Object.keys(this.sizes);
+        
+        for(const key of keys) {
+            if(this.state.width < this.sizes[key]) {
+                return colors[key];
+            } 
+            // To make sure that nothing breaks after 1800px
+            else if(this.state.width >= this.sizes.desktopPlus) {
+                return colors.desktopPlus;
+            }
+        }
+
+    }
+
+
+    render() {
+        const hidden = this.state.hidden;
+        const style = {
+            color: this.currentColor()
+        };
+
+        return (
+            <div 
+                className={ `device-info ${hidden ? "hidden" : ""}` } 
+                onClick={this.hideElement} >
+
+                <span style={style}>{this.state.width}px</span>
+                <span>{this.state.height}px</span>
+            </div>
+        );
+    }
+}
+
+
+export default DevInfo;
