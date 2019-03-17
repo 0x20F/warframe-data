8a0eeb4dc67d0f64c195e6ea7ac655de5fb088a2
Poly <alexhexan22@gmail.com>
2019-03-17 13:40:19 +0100

    Use new icons

--- src/index.js
+++ src/index.js
@@ -7,14 +7,16 @@ import ReactDOM     from "react-dom";
 
 // Fontawesome icons
 import { library }  from "@fortawesome/fontawesome-svg-core";
-import { faNewspaper, faHome, faExclamationTriangle }   from "@fortawesome/free-solid-svg-icons";
+import { faBookReader, faHome }   from "@fortawesome/free-solid-svg-icons";
 /**
  * Icon sizes:
  *  lg, xs, sm
  *  1x, 2x, 3x, 4x, 5x
  *  Add them as a size="" attribute on the component
  */
-library.add(faNewspaper, faHome, faExclamationTriangle);
+library.add(
+    faHome,
+    faBookReader);
 
 
 
