add5d6f687f61d9e1fe13be4b9fce17dc6488710
Poly <alexhexan22@gmail.com>
2019-03-20 13:47:32 +0100

    Tweaks for testing

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -21,14 +21,14 @@ class Home extends Component {
             <React.Fragment>
             
                 <Carousel className="home-carousel">
-                    <div>Item</div>
-                    <div>Item</div>
-                    <div>Item</div>
-                    <div>Item</div>
-                    <div>Item</div>
-                    <div>Item</div>
-                    <div>Item</div>
-                    <div>Item</div>
+                    <div>Item 1</div>
+                    <div>Item 2</div>
+                    <div>Item 3</div>
+                    <div>Item 4</div>
+                    <div>Item 5</div>
+                    <div>Item 6</div>
+                    <div>Item 7</div>
+                    <div>Item 8</div>
                 </Carousel>
 
             </React.Fragment>
