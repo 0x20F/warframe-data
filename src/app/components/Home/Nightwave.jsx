c4615a8fb5923c1aca506c45e109b5581332cd71
Poly <alexhexan22@gmail.com>
2019-03-25 20:13:11 +0100

    Nightwave alert filters now working

--- src/app/components/Home/Nightwave.jsx
+++ src/app/components/Home/Nightwave.jsx
@@ -5,10 +5,37 @@ import "@Styles/components/home/nightwave";
 
 class Nightwave extends Component {
 
+    constructor() {
+        super();
+
+        this.state = {
+            filter: "weekly"
+        };
+    }
+
+    setFilter = (filter) => {
+        this.setState({
+            filter: filter
+        })
+    };
+
     render() {
 
         const { data, className } = this.props;
         const challenges = data.activeChallenges.map((item, index) => {
+
+            switch(this.state.filter) {
+                case "daily":
+                    if(!item.isDaily) return;
+                    break;
+                case "weekly":
+                    if(item.isDaily || item.isElite) return;
+                    break;
+                case "elite":
+                    if(!item.isElite) return;
+                    break;
+            }
+
             return (
                 <div className="challenge" key={index}>
                     <div className="title">{item.title}</div>
@@ -23,9 +50,9 @@ class Nightwave extends Component {
                 <header>
                     <span className="title">Nightwave</span>
                     <span className="sort">
-                        <span className="bubble"> daily</span>
-                        <span className="bubble"> weekly</span>
-                        <span className="bubble"> elite</span>
+                        <span className="bubble" onClick={ () => this.setFilter("daily") }>daily</span>
+                        <span className="bubble" onClick={ () => this.setFilter("weekly") }>weekly</span>
+                        <span className="bubble" onClick={ () => this.setFilter("elite") }>elite</span>
                     </span>
                 </header>
 
