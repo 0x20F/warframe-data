e2cddb8eadacaff233c2ed713c052487235d8982
Poly <alexhexan22@gmail.com>
2019-03-26 17:29:49 +0100

    Update state every second with new time

--- src/app/components/Home/Nightwave/Challenge.jsx
+++ src/app/components/Home/Nightwave/Challenge.jsx
@@ -3,23 +3,34 @@ import React, { Component } from "react";
 
 class Challenge extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         this.state = {
-            interval: null
+            data: this.props.itemData,
+            time: this.calculateTime(this.props.itemData)
         }
     }
 
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.updateState(this.calculateTime(this.state.data));
+        }, 1000);
+    }
 
-    updateStateInterval = (interval) => {
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+
+    updateState = (time) => {
         this.setState({
-            interval: interval
+            time: time
         });
     }
 
 
-    calculateTime(itemData) {
+    calculateTime = (itemData) => {
         const dateInfo = {
             d: 1E3 * 60 * 60 * 24,
             h: 1E3 * 60 * 60,
@@ -32,33 +43,26 @@ class Challenge extends Component {
         const distance = expiry - now;
 
         let key = Object.keys(dateInfo).find(key => dateInfo[key] <= distance);
-        
+
         return `${Math.floor(distance / dateInfo[key]) || "expired"}${key || ""}`;
     }
 
 
     render() {
 
-        const { itemData } = this.props;
-        const current = this.calculateTime(itemData);
-
-
-        /**
-             * Some titles show up in descriptions aswell
-             * prevent that.
-             */
-        const result = itemData.desc.includes(itemData.title);
+        const { data, time } = this.state;
+        const result = data.desc.includes(data.title); // If title and description are similar only add title
 
         return (
             <div className="challenge">
 
-                <div className="title">{ itemData.title }</div>
-                
-                { !result && <div className="description">{ itemData.desc }</div> }
-            
+                <div className="title">{ data.title }</div>
+
+                { !result && <div className="description">{ data.desc }</div> }
+
                 <div className="challenge-info">
-                    <div className="rep">{ itemData.reputation } rep</div>
-                    <div className="time-left">{ current }</div>
+                    <div className="rep">{ data.reputation } rep</div>
+                    <div className="time-left">{ time }</div>
                 </div>
 
             </div>
