2bac39025c929c1a3a1141a45e961bbcacb0d447
Poly <alexhexan22@gmail.com>
2019-03-29 14:57:22 +0100

    Start building second home page component

---
+++ src/app/components/Home/QuickInfo/QuickInfo.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+
+class QuickInfo extends Component {
+
+    render() {
+        return (
+            <div className="quick-info">
+                Grid Here...
+            </div>
+        );
+    }
+}
+
+export default QuickInfo;
