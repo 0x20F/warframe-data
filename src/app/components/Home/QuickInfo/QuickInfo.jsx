5511142d71abb4aaf418419b026e58bc9ed5d7bf
Poly <alexhexan22@gmail.com>
2019-03-31 10:46:36 +0200

    Begin building the next component

--- src/app/components/Home/QuickInfo/QuickInfo.jsx
+++ src/app/components/Home/QuickInfo/QuickInfo.jsx
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 
+import Carousel from "@Components/Carousel/Carousel";
+
+
+import "@Styles/components/home/quickinfo";
 
 class QuickInfo extends Component {
 
+    constructor() {
+        super();
+    }
+
+
     render() {
+        const { className } = this.props;
+
         return (
-            <div className="quick-info">
-                Grid Here...
+            <div className={ `quick-info ${className}` }>
+
+                <div className="cycles shadow">
+
+                    
+
+                </div>
+
             </div>
         );
     }
