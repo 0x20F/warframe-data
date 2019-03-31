fc52c9ce3c5d735c907939838e960af9b493a1a6
Poly <alexhexan22@gmail.com>
2019-03-31 10:48:09 +0200

    Small tests

--- src/app/components/Carousel/CarouselContainer.jsx
+++ src/app/components/Carousel/CarouselContainer.jsx
@@ -4,6 +4,7 @@ import "@Styles/components/carousel/carousel-container";
 
 const CarouselContainer = (props) => {
 
+    // TODO: Translate this by the width of the child element
     const style = {
         transform: `translateX(calc(var(--slot-basis-n) * ${props.position}))`
     }
